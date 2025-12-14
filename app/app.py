cacd130a2c4bfe3de6a2a2bd3af86e7428e34c2d
Akira Agha Nugroho <akiraaghan999@gmail.com>
2025-12-14 21:30:45 +0700

    Update app.py import os

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -112,3 +114,8 @@ def predict():
 # === Run server ===
 # if __name__ == "__main__":
 #     app.run(debug=True, port=5000)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
